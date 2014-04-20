279236be85d38916ba067904e8125f97d53c4394
Zach Kelling <zk@monoid.io>
2014-04-19 22:37:43 -0500

    Update ellipsis.sh

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 mod.install() {
-    backup ~/.module
-    backup ~/.xmobarrc
-    backup ~/.xinitrc
+    ellipsis.backup ~/.module
+    ellipsis.backup ~/.xmobarrc
+    ellipsis.backup ~/.xinitrc
     ln -s $mod_path ~/.xmonad
     ln -s $mod_path/xmobarrc ~/.xmobarrc
     ln -s $mod_path/xinitrc ~/.xinitrc
